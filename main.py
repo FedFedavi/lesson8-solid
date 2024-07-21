dfdd42b5b5217acd1739529287b6ecca9eea9873
FedFedavi <kykypyk@gmail.com>
2024-07-21 16:11:24 +0400

    new main code dz

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
-# class UserManager():
-#     def __init__(self, user):
-#         self.user = user
-#
-#     def change_user_name(self, user_name):
-#         self.user = user_name
-#
-#     def save_user(self):
-#         file = open("user.txt", "a")
-#         file.write(self.user)
-#         file.close()
-# Выше код без Solo (SOLID)
-# Ниже код с Solo (Solid)
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Sword(Weapon):
+    def attack(self):
+        print("Боец наносит удар мечом")
+
+class Bow(Weapon):
+    def attack(self):
+        print("Боец стреляет из лука")
+
+class Fighter:
+    def __init__(self, name, weapon: Weapon):
+        self.name = name
+        self.weapon = weapon
+
+    def change_weapon(self, weapon: Weapon):
+        self.weapon = weapon
+        print(f"{self.name} выбирает новое оружие.")
+
+    def fight(self):
+        print(f"{self.name} наносит удар ")
+        self.weapon.attack()
 
 
-class User():
-    def __init__(self, user):
-        self.user = user
+class Monster:
+    def __init__(self, name):
+        self.name = name
+
+    def defeat(self):
+        print(f"{self.name} побежден!")
 
 
-class UserNameChanger():
-    def __init__(self, user):
-        self.user = user
+    # Создаем оружие
+sword1 = Sword()
+bow1 = Bow()
 
-    def change_name(self, new_name):
-        self.user = new_name
+    # Создаем бойца и монстра
+fighter = Fighter("Боец", sword1)
+monster = Monster("Монстр")
 
 
-class SaveUser():
-    def __init__(self, user):
-        self.user = user
 
-    def save(self):
-        file = open("user.txt", "a")
-        file.write(self.user)
-        file.close()
+    # Боец выбирает лук и атакует монстра
+fighter.change_weapon(bow1)
+fighter.fight()
+monster.defeat()
+
+    # Боец выбирает меч и атакует монстра
+fighter.change_weapon(sword1)
+fighter.fight()
+monster.defeat()
+
+
